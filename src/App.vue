0bf58c409c018f8e14dc8bee7d30a0bd48e04a34
iamcoderloser <642872728@qq.com>
2019-09-19 21:19:15 +0800

    导入初始工程

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <HelloWorld>
+      <span slot='slotc'>槽口C</span>
+			<span slot='slotb'>槽口B</span>
+			槽口A
+    </HelloWorld>
+    <div>{{loading}}xxx</div>
+
+    <el-button type="primary" :loading="loading">加载中</el-button>
+  </div>
+
+
+</template>
+
+
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      loading:true
+    }
+  },
+  mounted() {
+    var that = this;
+    setTimeout(() => {
+      that.loading = false;
+    }, 5000);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <h2>test</h2>
+			<slot name='slotb'></slot>
+			<slot></slot>
+			<slot name='slotc'></slot>		
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+
+
